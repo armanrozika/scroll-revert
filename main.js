19ce510b149b5fcf7eebfab908086fe3d1722bfa
Lalu Arman Rozika <lalu.arman@kompas.com>
2018-10-10 14:43:36 +0700

    t

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+// let container = document.querySelector('.container');
+// let wrap = document.querySelector('.wrap');
+// let child = document.querySelectorAll('.child');
+
+// let arr = [];
+
+// for(let i=0; i<child.length; i++){
+// 	arr.push(child[i].offsetHeight);
+// };
+
+// console.log(arr)
+// console.log(child[0].offsetHeight)
+// console.log(child[1].offsetHeight)
+// console.log(child[2].offsetHeight)
+// console.log(child[3].offsetHeight)
+
+// function sum(total, num){
+// 	return total + num;
+// }
+
+// let scrollY = window.scrollY;
+// container.style.bottom = -scrollY
+
+// let childHeightSum = arr.reduce(sum);
+
+// container.style.height = `${childHeightSum}px`
+// wrap.style.height = `${childHeightSum}px`
+
+// window.addEventListener('scroll', function(){
+// 	let scrollY = window.scrollY;
+// 	container.style.bottom = -scrollY
+// });
+
+
+setTimeout(function(){
 let container = document.querySelector('.container');
 let wrap = document.querySelector('.wrap');
 let child = document.querySelectorAll('.child');
@@ -5,19 +40,30 @@ let child = document.querySelectorAll('.child');
 let arr = [];
 
 for(let i=0; i<child.length; i++){
-	arr.push(child[i].clientHeight);
+	arr.push(child[i].offsetHeight);
 };
+
+console.log(arr)
+console.log(child[0].offsetHeight)
+console.log(child[1].offsetHeight)
+console.log(child[2].offsetHeight)
+console.log(child[3].offsetHeight)
 
 function sum(total, num){
 	return total + num;
 }
 
+
+
 let childHeightSum = arr.reduce(sum);
 
-container.style.height = `${childHeightSum}px`
-wrap.style.height = `${childHeightSum}px`
+container.style.height = `${childHeightSum-100}px`
+wrap.style.height = `${childHeightSum-100}px`
+
 
 window.addEventListener('scroll', function(){
 	let scrollY = window.scrollY;
 	container.style.bottom = -scrollY
-})
+});
+
+},2000)
